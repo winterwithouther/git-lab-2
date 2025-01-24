616cd5347fe311c8401548f560af5bfbac4798d0
Eman Gurung <egurung1117@gmail.com>
2025-01-24 15:33:25 -0500

    first commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Eman Gurung
+ *        Date: 1/24/2025
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
